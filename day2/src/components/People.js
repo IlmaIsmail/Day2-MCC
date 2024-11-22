8fc796ad6f5ea74a6d34db211a1684ef62185e21
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2024-11-22 12:53:49 +0000

    modifying People

---
+++ day2/src/components/People.js
@@ -0,0 +1,16 @@
+import '../assets/main.css';
+
+export default function People({detail}){
+    return(
+        <div className="maincard">
+            <div>
+                <img className="profile" src={'https://i.imgur.com/'+detail.imageId+'s.jpg'} alt={detail.name}/>
+            </div>
+            <div className="details">
+                <p>{detail.name}</p>
+                <p>{detail.profession}</p>
+                <p>{detail.accomplishment}</p>
+            </div>
+        </div>
+    )
+}
