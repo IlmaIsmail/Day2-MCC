0bc04abe82205b4e9d67b2f82b493dc57fae0bb7
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2024-11-22 12:46:19 +0000

    defining Header

---
+++ day2/src/components/Header.js
@@ -0,0 +1,10 @@
+import '../assets/main.css';
+
+export default function Header(){
+    return(
+        <>
+        <h1 className="header">Learning React!</h1>
+        <hr/>
+        </>
+    )
+}
